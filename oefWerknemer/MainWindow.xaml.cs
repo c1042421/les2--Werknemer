ea6e6bb51cc9716a7684c617788d638f5b879dd9
c1042421 <c1042421@G-D101-17>
2017-09-28 11:45:07 +0200

    added updateView method

--- oefWerknemer/MainWindow.xaml.cs
+++ oefWerknemer/MainWindow.xaml.cs
@@ -24,5 +24,51 @@ namespace oefWerknemer
         {
             InitializeComponent();
         }
+
+        private void txtToevoegen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void radioButtonClicked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = (RadioButton)sender;
+
+            updateViewFor(rb.Name);
+        }
+
+        private void updateViewFor(string type)
+        {
+            switch (type)
+            {
+                case WerknemerType.CommissieWerker:
+                    stpAantal.Visibility = Visibility.Visible;
+                    stpCommissie.Visibility = Visibility.Visible;                    
+                    break;
+                case WerknemerType.StukWerker:
+                    stpAantal.Visibility = Visibility.Visible;
+                    stpCommissie.Visibility = Visibility.Hidden;
+                    break;
+                case WerknemerType.Uurwerker:
+                    stpAantal.Visibility = Visibility.Visible;
+                    stpCommissie.Visibility = Visibility.Hidden;
+                    break;
+                case WerknemerType.Werknemer:
+                    stpAantal.Visibility = Visibility.Hidden;
+                    stpCommissie.Visibility = Visibility.Hidden;
+                    break;
+                default:
+                    break;
+            }
+        }
+
     }
+}
+
+public sealed class WerknemerType
+{
+    public const string CommissieWerker = "rbCommissieWerker";
+    public const string StukWerker = "rbStukWerker";
+    public const string Uurwerker = "rbUurWerker";
+    public const string Werknemer = "rbWerknemer";
 }
